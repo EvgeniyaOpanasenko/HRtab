02c8d92cf50db92a12513b72784ffae95c3c0000
EvgeniyaOpanasenko <evgeniya.opanasenko@outlook.com>
2018-03-10 07:14:16 -0200

    innit commit

---
+++ src/main/java/com/example/EmployeeTab/EmployeeTabApplication.java
@@ -0,0 +1,12 @@
+package com.example.EmployeeTab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeTabApplication.class, args);
+	}
+}
